6066b566f11fb70437c03041f5728cbdfc204827
Anton Fedenko <crays01234@gmail.com>
2024-06-25 19:45:07 +0700

    Добавил обводку слоя шапки. Добавил визуализацию действий кнопки. Добавил переход на главную по нажатию на логотип.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,25 @@
 import { Button } from '../Button/Button'
 import styles from './Header.module.scss'
 import logo from '@/assets/images/svg/logo.svg'
+import { useNavigate } from "react-router-dom";
 
 export const Header = () => {
+  const navigate = useNavigate();
+
   return (
     <div className={styles.header_layout}>
-      <div className={styles.header_logo_block}>
-        <img className={styles.logo} src={logo} />
-        <div className={styles.header_logo_blocK_text}>
-          <h1 className={styles.logo_text}>Project Help</h1>
-          <span className={styles.logo_subtext}>Каждый листок — вклад в добро</span>
+      <div className={styles.header_container}>
+        <div className={styles.header_logo_block} onClick={() => navigate("/")}>
+          <img className={styles.logo} src={logo} />
+          <div className={styles.header_logo_blocK_text}>
+            <h1 className={styles.logo_text}>Project Help</h1>
+            <span className={styles.logo_subtext}>Каждый листок — вклад в добро</span>
+          </div>
         </div>
-      </div>
-      <div className={styles.header_block_btns}>
-        <Button typeStyle='light' text='Регистрация' />
-        <Button typeStyle='dark' text='Войти' />
+        <div className={styles.header_block_btns}>
+          <Button typeStyle='light' text='Регистрация' />
+          <Button typeStyle='dark' text='Войти' />
+        </div>
       </div>
     </div>
   )
